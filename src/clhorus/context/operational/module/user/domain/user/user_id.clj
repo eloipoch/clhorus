bcfa11b51654e5990b10c76c5603912384e49a3d
Jordi Llonch <llonch.jordi@gmail.com>
2015-03-08 00:01:12 +0100

    validate uuid when create user-id vo

--- src/clhorus/context/operational/module/user/domain/user/user_id.clj
+++ src/clhorus/context/operational/module/user/domain/user/user_id.clj
@@ -1,7 +1,9 @@
-(ns clhorus.context.operational.module.user.domain.user.user-id)
+(ns clhorus.context.operational.module.user.domain.user.user-id
+  (:require [clj-uuid :as uuid]))
 
 (defrecord UserId [id])
 
 (defn create-user-id [id]
+  {:pre [(uuid/uuid-string? id)]}
   (UserId. id)
   )
