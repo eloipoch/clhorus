0254377a522c274b35c2bfe5b2030dbb96239add
Jordi Llonch <llonch.jordi@gmail.com>
2015-03-08 15:06:59 +0100

    improve uuid util

--- src/clhorus/util/uuid.clj
+++ src/clhorus/util/uuid.clj
@@ -1,17 +1,8 @@
-(ns clhorus.util.uuid)
+(ns clhorus.util.uuid
+  (:require [clj-uuid :as uuid])
+  (:import [java.util UUID]))
 
-(require '[clj-uuid :as uuid])
-
-(defn- uuid-as-byte-array [uuid]
-  (let [u uuid                                              ; uuid4
-        ^long lo (.getLeastSignificantBits u)               ; least significant bits
-        ^long hi (.getMostSignificantBits u)]               ; most significant bits (left-most bits)
-    (-> (java.nio.ByteBuffer/allocate 16)                   ; http://docs.oracle.com/javase/6/docs/api/java/util/UUID.html
-        (.putLong hi)
-        (.putLong lo)
-        (.array))))
-
-(defn uuid-to-binary [uuid-string]
-  (-> (uuid/as-uuid uuid-string)
-      (uuid-as-byte-array)
-      ))
+(defn uuid-to-binary [^UUID uuid]
+  (-> (uuid/to-byte-vector uuid)
+      (byte-array))
+  )
