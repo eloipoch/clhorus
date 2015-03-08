ee54ef4724e9656df46b64417c233f3ae06ce83c
Jordi Llonch <llonch.jordi@gmail.com>
2015-03-08 22:30:43 +0100

    improve korma repository

--- src/clhorus/context/operational/module/user/infrastructure/persistence/user/user_mapping.clj
+++ src/clhorus/context/operational/module/user/infrastructure/persistence/user/user_mapping.clj
@@ -1,6 +1,6 @@
 (ns clhorus.context.operational.module.user.infrastructure.persistence.user.user-mapping
   (:use clhorus.context.operational.infrastructure.persistence.korma.connection)
-  (:use clhorus.util.korma)
+  (:use clhorus.util.uuid)
   (:import (clhorus.context.operational.module.user.domain.user.user User)))
 
 (use 'korma.core)
@@ -11,11 +11,10 @@
            (database operational-db)
            (table :user)
            (entity-fields :id)
-           (prepare (translate-uuid-to-binary :id))
            )
 
 (defn user-to-korma [^User user]
-  {:id (-> user :user-id :id)}
+  {:id (-> user :user-id :id (uuid-to-binary))}
   )
 
 ; @todo korma-to-user
--- src/clhorus/util/korma.clj
+++
@@ -1,7 +0,0 @@
-(ns clhorus.util.korma
-  (:use clhorus.util.uuid))
-
-(defn translate-uuid-to-binary [field]
-  (fn [{id field :as v}]
-    (assoc v field (uuid-to-binary id))
-    ))
