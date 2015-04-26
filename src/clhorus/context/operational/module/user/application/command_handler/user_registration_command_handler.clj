57f528225e6c1cbbad2e80acadf8fe09a16c10d3
Jordi Llonch <llonch.jordi@gmail.com>
2015-04-26 17:11:44 +0200

    some refactors

--- src/clhorus/context/operational/module/user/application/command_handler/user_registration_command_handler.clj
+++ src/clhorus/context/operational/module/user/application/command_handler/user_registration_command_handler.clj
@@ -1,10 +1,9 @@
 (ns clhorus.context.operational.module.user.application.command-handler.user-registration-command-handler
   (:require [clhorus.context.operational.module.user.contract.command.user-registration-command]
             [clhorus.context.operational.module.user.application.service.user-registrator :refer [register-user]]
-            [clhorus.context.operational.module.user.domain.user.user-id :refer [create-user-id]])
-  (:import (clhorus.context.operational.module.user.contract.command.user_registration_command UserRegistrationCommand)))
+            [clhorus.context.operational.module.user.domain.user.user-id :refer [create-user-id]]))
 
-(defn handle [^UserRegistrationCommand repository publisher command]
+(defn handle [repository publisher command]
   (-> (:user-id command)
       (create-user-id)
       (register-user repository publisher))
--- src/clhorus/context/operational/module/user/application/service/user_registrator.clj
+++ src/clhorus/context/operational/module/user/application/service/user_registrator.clj
@@ -1,8 +1,6 @@
 (ns clhorus.context.operational.module.user.application.service.user-registrator
   (:use clhorus.context.operational.module.user.domain.user.user)
-  (:use clhorus.context.operational.module.user.domain.user.user-repository)
-  ;(:require [clhorus.infrastructure.domain-event-publisher.vertx :refer [publish]])
-  )
+  (:use clhorus.context.operational.module.user.domain.user.user-repository))
 
 (defn register-user [user-id repository publisher]
   (let [[user domain-events] (create-user user-id)]
