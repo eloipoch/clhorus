74e53370227da49248cade3a6ba28c9b3dcb440f
Jordi Llonch <llonch.jordi@gmail.com>
2015-11-07 10:14:48 +0100

    first approach to a rabbitmq worker

--- src/clhorus/context/analytics/core.clj
+++ src/clhorus/context/analytics/core.clj
@@ -2,10 +2,14 @@
   (:require [com.stuartsierra.component :as component]
             [clhorus.context.analytics.config :refer [analytics-config]]
             [clhorus.context.analytics.infrastructure.persistence.korma-component :refer [new-database]]
+            [clhorus.context.analytics.module.user.infrastructure.worker.domain-event.worker :refer [new-domain-event-worker]]
             [clhorus.context.analytics.module.user.user-module-component :refer [new-user-module-system]]))
 
 (defn new-context-analytics-system []
   (component/system-map
     :database-analytics (new-database (:database analytics-config))
-    :user-module (-> (new-user-module-system) (component/using [:database-analytics
-                                                                :domain-event-publisher]))))
+    :domain-event-worker (-> (new-domain-event-worker)
+                             (component/using [:domain-event-publisher]))
+    :user-module (-> (new-user-module-system)
+                     (component/using [:database-analytics
+                                       :domain-event-publisher]))))
